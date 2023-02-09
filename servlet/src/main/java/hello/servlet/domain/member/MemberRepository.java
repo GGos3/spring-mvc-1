c4488433cd09fe1ae95a561ec2e91fe1140d702e
JangFeel <janpll0421@gmail.com>
2023-02-09 10:13:46 +0900

    회원 관리 예제 생성

--- servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package hello.servlet.domain.member;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +21,25 @@ public class MemberRepository {
     public static MemberRepository getInstance() {
         return instance;
     }
-    public MemberRepository() {
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
